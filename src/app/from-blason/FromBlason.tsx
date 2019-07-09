a4e00c755d7a00d0e0abaa4e8295e44ace581af4
Benoit Lemoine <blemoine@mnubo.com>
2019-07-09 18:03:43 -0400

    refactor(ordinary): extract ordinary form

--- src/app/from-blason/FromBlason.tsx
+++ src/app/from-blason/FromBlason.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { CoatsOfArmsDisplay } from './CoatsOfArmsDisplay';
-import { argent, Blason, ermine, gules, Ordinary, Tincture } from '../model/blason';
+import { Blason, ermine, gules, Ordinary, Tincture } from '../model/blason';
 import { TinctureSelect } from './TinctureSelect';
-import { OrdinaryNameSelect } from './OrdinaryNameSelect';
+import { OrdinaryForm } from './OrdinaryForm';
 
 export const FromBlason = () => {
   const [blason, setBlason] = useState<Blason>({
@@ -18,28 +18,13 @@ export const FromBlason = () => {
     setBlason({ ...blason, field });
   }
 
-  function changeOrdinary(ordinary: Ordinary['name'] | null) {
+  function ordinaryChange(ordinary: Ordinary | null) {
     if (ordinary) {
-      const tincture = blason.ordinary ? blason.ordinary.tincture : argent;
-      setBlason({ ...blason, ordinary: { name: ordinary, tincture } });
+      setBlason({ ...blason, ordinary });
     } else {
       const newBlason = { ...blason };
       delete newBlason.ordinary;
       setBlason(newBlason);
-    }
-  }
-
-  function ordinaryTinctureChange(ordinaryTincture: Tincture) {
-    if (blason.ordinary) {
-      setBlason({
-        ...blason,
-        ordinary: {
-          ...blason.ordinary,
-          tincture: ordinaryTincture,
-        },
-      });
-    } else {
-      throw new Error('This function should not be called if there is no ordinary');
     }
   }
 
@@ -50,25 +35,8 @@ export const FromBlason = () => {
           <label>Select your field</label>
           <TinctureSelect tincture={blason.field} tinctureChange={fieldChange} />
         </div>
-        <div className="row">
-          <div className="col">
-            <div className="form-group">
-              <label>Select your ordinary</label>
-              <OrdinaryNameSelect
-                ordinary={blason.ordinary ? blason.ordinary.name : null}
-                ordinaryChange={changeOrdinary}
-              />
-            </div>
-          </div>
-          <div className="col">
-            {blason.ordinary && (
-              <div className="form-group">
-                <label>Select the tincture of the ordinary</label>
-                <TinctureSelect tincture={blason.ordinary.tincture} tinctureChange={ordinaryTinctureChange} />
-              </div>
-            )}
-          </div>
-        </div>
+
+        <OrdinaryForm ordinary={blason.ordinary || null} ordinaryChange={ordinaryChange} />
       </div>
       <div className="col-md-12 col-lg-6">
         <CoatsOfArmsDisplay blason={blason} />
---
+++ src/app/from-blason/OrdinaryForm.tsx
@@ -0,0 +1,46 @@
+import { OrdinaryNameSelect } from './OrdinaryNameSelect';
+import { TinctureSelect } from './TinctureSelect';
+import * as React from 'react';
+import { argent, Ordinary, Tincture } from '../model/blason';
+
+type Props = { ordinary: Ordinary | null; ordinaryChange: (ordinary: Ordinary | null) => void };
+export const OrdinaryForm = ({ ordinary, ordinaryChange }: Props) => {
+  function changeOrdinary(ordinaryName: Ordinary['name'] | null) {
+    if (ordinaryName) {
+      const tincture = ordinary ? ordinary.tincture : argent;
+      ordinaryChange({ name: ordinaryName, tincture });
+    } else {
+      ordinaryChange(null);
+    }
+  }
+
+  function ordinaryTinctureChange(ordinaryTincture: Tincture) {
+    if (ordinary) {
+      ordinaryChange({
+        ...ordinary,
+        tincture: ordinaryTincture,
+      });
+    } else {
+      throw new Error('This function should not be called if there is no ordinary');
+    }
+  }
+
+  return (
+    <div className="row">
+      <div className="col">
+        <div className="form-group">
+          <label>Select your ordinary</label>
+          <OrdinaryNameSelect ordinary={ordinary ? ordinary.name : null} ordinaryChange={changeOrdinary} />
+        </div>
+      </div>
+      <div className="col">
+        {ordinary && (
+          <div className="form-group">
+            <label>Select the tincture of the ordinary</label>
+            <TinctureSelect tincture={ordinary.tincture} tinctureChange={ordinaryTinctureChange} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
