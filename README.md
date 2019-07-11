3281ea77b415459fd2be66f5073fde72c1bb6762
Benoit Lemoine <blemoine@mnubo.com>
2019-07-11 15:49:02 -0400

    fix(tincture): display fur example in select box

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Blason part courtesy of http://www.vikinganswerlady.com/Stars/Heraldry_SVG_Image
 TODO
 ---
 
-Tincture selector -> ajouter une icone pour les furs
 Furs dans les ordinary ->  les centrer correctement
 
 Add some tests
--- src/app/from-blason/TinctureSelect.tsx
+++ src/app/from-blason/TinctureSelect.tsx
@@ -2,6 +2,8 @@ import Select, { components } from 'react-select';
 import * as React from 'react';
 import { OptionProps } from 'react-select/src/components/Option';
 import { Tincture, tinctures } from '../model/tincture';
+import { ErmineDisplay } from './coats-of-arms-parts/ErmineDisplay';
+import { VairDisplay } from './coats-of-arms-parts/VairDisplay';
 
 type Props = { tincture: Tincture; tinctureChange: (t: Tincture) => void };
 
@@ -15,17 +17,35 @@ const Option = (props: OptionProps<Tincture>) => {
       >
         {props.label}
       </span>
+
       <span
         style={{
           marginLeft: '5px',
           verticalAlign: 'middle',
           display: 'inline-block',
-          border: '1px solid #333',
-          backgroundColor: props.data.color,
+          border: '1px solid #777',
+          backgroundColor: props.data.color || 'white',
           width: '15px',
           height: '15px',
+          lineHeight: '15px',
+          overflow: 'hidden',
+          boxSizing: 'content-box',
+          textAlign: 'center',
+          padding: '1px',
         }}
-      />
+      >
+        {props.data.name === 'ermine' ? (
+          <svg width={12.5} height={15} viewBox={`0 0 200 240`}>
+            <ErmineDisplay width={200} height={240} />
+          </svg>
+        ) : props.data.name === 'vair' ? (
+          <svg width={15} height={15} viewBox={`0 0 200 200`}>
+            <VairDisplay width={200} height={200} />
+          </svg>
+        ) : (
+          <>&nbsp;</>
+        )}
+      </span>
     </components.Option>
   );
 };
