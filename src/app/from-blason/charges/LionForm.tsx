2a62a90bb6abcdc4219a9d9b511b9197b33f3d81
Benoit Lemoine <blemoine@mnubo.com>
2019-07-14 10:18:50 -0400

    feat(lion): add support for head position

--- src/app/from-blason/charges/LionForm.tsx
+++ src/app/from-blason/charges/LionForm.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Lion } from '../../model/charge';
+import { Lion, LionHead, lionHeads } from '../../model/charge';
 import { TinctureSelect } from '../TinctureSelect';
 import { Tincture } from '../../model/tincture';
+import Select from 'react-select';
 type Props = { charge: Lion; chargeChange: (lion: Lion) => void };
 
 export const LionForm = ({ charge, chargeChange }: Props) => {
@@ -19,6 +20,17 @@ export const LionForm = ({ charge, chargeChange }: Props) => {
     });
   }
 
+  let defaultHeadValue = { value: null, label: 'None' };
+  const headPostures = [defaultHeadValue, ...lionHeads.map((head) => ({ value: head, label: head }))];
+  const selectedHead = headPostures.find(({value}) => value === charge.head) || headPostures;
+
+  function headPostureChange(head: LionHead | null) {
+    chargeChange({
+      ...charge,
+      head,
+    });
+  }
+
   return (
     <div className="row">
       <div className="col">
@@ -31,6 +43,16 @@ export const LionForm = ({ charge, chargeChange }: Props) => {
         <div className="form-group">
           <label>Select the tincture of the claws and tongue</label>
           <TinctureSelect tincture={charge.armedAndLangued} tinctureChange={armedLanguedTinctureChange} />
+        </div>
+      </div>
+      <div className="col">
+        <div className="form-group">
+          <label>Select the head posture</label>
+          <Select
+            options={headPostures}
+            value={selectedHead}
+            onChange={(t: any) => headPostureChange(t.value === 'None' ? null : t.value)}
+          />
         </div>
       </div>
     </div>
--- src/app/from-blason/coats-of-arms-parts/charge/lion/LionDisplay.tsx
+++ src/app/from-blason/coats-of-arms-parts/charge/lion/LionDisplay.tsx
@@ -1,19 +1,58 @@
-import { Lion } from '../../../../model/charge';
+import { Lion, LionHead } from '../../../../model/charge';
 import SvgLionRampant from './SvgLionRampant';
 import * as React from 'react';
 import { Tincture } from '../../../../model/tincture';
+import { cannotHappen } from '../../../../../utils/cannot-happen';
+import { DefaultLionHead } from './SvgDefaultLionHead';
+import { ReactNode } from 'react';
+import SvgGuardantLionHead from './SvgGuardantLionHead';
+import SvgRegardantLionHead from './SvgRegardantLionHead';
 
 type Props = { charge: Lion; width: number; height: number; fillFromTincture: (tincture: Tincture) => string };
 export const LionDisplay = ({ charge, width, height, fillFromTincture }: Props) => {
-  return (
-    <SvgLionRampant
-      width={width * 0.8}
-      height={height * 0.8}
-      x={width * 0.1}
-      y={0}
-      mainFill={fillFromTincture(charge.tincture)}
-      tongueFill={fillFromTincture(charge.armedAndLangued)}
-      clawFill={fillFromTincture(charge.armedAndLangued)}
-    />
-  );
+  if (charge.attitude === 'rampant') {
+    const armedAndLanguedFill = fillFromTincture(charge.armedAndLangued);
+    const mainFill = fillFromTincture(charge.tincture);
+    const head = lionHead(charge.head, armedAndLanguedFill, mainFill);
+
+    return (
+      <SvgLionRampant
+        width={width * 0.8}
+        height={height * 0.8}
+        x={width * 0.1}
+        y={0}
+        mainFill={mainFill}
+        head={head}
+        clawFill={armedAndLanguedFill}
+      />
+    );
+  } else if (charge.attitude === 'passant') {
+    throw new Error('TODO unsupported' + charge.attitude);
+  } else if (charge.attitude === 'statant') {
+    throw new Error('TODO unsupported' + charge.attitude);
+  } else if (charge.attitude === 'salient') {
+    throw new Error('TODO unsupported' + charge.attitude);
+  } else if (charge.attitude === 'sejant') {
+    throw new Error('TODO unsupported' + charge.attitude);
+  } else if (charge.attitude === 'sejant-erect') {
+    throw new Error('TODO unsupported' + charge.attitude);
+  } else if (charge.attitude === 'couchant') {
+    throw new Error('TODO unsupported' + charge.attitude);
+  } else if (charge.attitude === 'dormant') {
+    throw new Error('TODO unsupported' + charge.attitude);
+  } else {
+    return cannotHappen(charge.attitude);
+  }
 };
+
+function lionHead(head: LionHead | null, armedAndLanguedFill: string, mainFill: string): ReactNode {
+  if (head === null) {
+    return <DefaultLionHead tongueFill={armedAndLanguedFill} mainFill={mainFill} />;
+  } else if (head === 'guardant') {
+    return <SvgGuardantLionHead tongueFill={armedAndLanguedFill} mainFill={mainFill} />;
+  } else if (head === 'regardant') {
+    return <SvgRegardantLionHead tongueFill={armedAndLanguedFill} mainFill={mainFill} />;
+  } else {
+    return cannotHappen(head);
+  }
+}
