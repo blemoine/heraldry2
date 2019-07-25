fca346338e861e95bdec40cab9987ffda338e3b2
Benoit Lemoine <blemoine@mnubo.com>
2019-07-25 13:29:28 -0400

    refactor(chargeParser): extract lion and eagle

--- src/app/blason-parser/chargeParser.ts
+++ src/app/blason-parser/chargeParser.ts
@@ -19,70 +19,76 @@ import { gules } from '../model/tincture';
 import { cannotHappen } from '../../utils/cannot-happen';
 import { tinctureParserFromName } from './tinctureParser';
 
+const lionParser = (count: 1 | 2 | 3): P.Parser<Lion> => {
+  const attitudeParser: P.Parser<LionAttitude> = buildAltParser(lionAttitudes, identity);
+  const headParser: P.Parser<LionHead> = buildAltParser(lionHeads, identity);
+  const tailParser: P.Parser<LionTail> = buildAltParser(lionTails, identity);
+
+  const lionNameParser = count === 1 ? P.regex(/lion/i) : P.regex(/lions/i);
+  const lionParser: P.Parser<Lion> = P.seq(
+    lionNameParser.skip(P.whitespace).then(attitudeParser),
+    P.whitespace.then(headParser).fallback(null),
+    P.whitespace
+      .then(P.regex(/tail/i))
+      .then(P.whitespace)
+      .then(tailParser)
+      .fallback(null),
+    count === 1
+      ? P.of({ count })
+      : P.whitespace.then(P.regex(/in pale/i).result('pale' as const)).map((disposition) => ({ count, disposition })),
+    P.whitespace.then(tinctureParserFromName),
+    P.whitespace
+      .then(P.regex(/armed and langued/i))
+      .then(P.whitespace)
+      .then(tinctureParserFromName)
+      .fallback(null)
+  ).map(([attitude, head, tail, countAndDisposition, tincture, armedAndLangued]) => {
+    return {
+      name: 'lion',
+      attitude,
+      tincture,
+      armedAndLangued: armedAndLangued || gules,
+      tail,
+      head,
+      countAndDisposition,
+    };
+  });
+
+  return lionParser;
+};
+
+const eagleParser = (count: 1 | 2 | 3) => {
+  const eagleNameParser = count === 1 ? P.regex(/eagle/i) : P.regex(/eagles/i);
+  const eagleAttitudeParser: P.Parser<EagleAttitude> = buildAltParser(eagleAttitudes, identity);
+
+  return P.seq(
+    eagleNameParser.skip(P.whitespace).then(eagleAttitudeParser),
+    P.whitespace.then(tinctureParserFromName),
+    P.whitespace
+      .then(P.regex(/beaked and armed/i))
+      .then(P.whitespace)
+      .then(tinctureParserFromName)
+      .fallback(null)
+  ).map(
+    ([attitude, tincture, beakedAndArmed]): Eagle => {
+      return {
+        name: 'eagle',
+        attitude,
+        tincture,
+        beakedAndArmed: beakedAndArmed || tincture,
+      };
+    }
+  );
+};
+
 export function chargeParser(): P.Parser<Charge> {
   const chargesParser = (count: 1 | 2 | 3): P.Parser<Charge> =>
     P.alt(
       ...charges.map((charge) => {
         if (charge === 'lion') {
-          const attitudeParser: P.Parser<LionAttitude> = buildAltParser(lionAttitudes, identity);
-          const headParser: P.Parser<LionHead> = buildAltParser(lionHeads, identity);
-          const tailParser: P.Parser<LionTail> = buildAltParser(lionTails, identity);
-
-          const lionNameParser = count === 1 ? P.regex(/lion/i) : P.regex(/lions/i);
-          const lionParser: P.Parser<Lion> = P.seq(
-            lionNameParser.skip(P.whitespace).then(attitudeParser),
-            P.whitespace.then(headParser).fallback(null),
-            P.whitespace
-              .then(P.regex(/tail/i))
-              .then(P.whitespace)
-              .then(tailParser)
-              .fallback(null),
-            count === 1
-              ? P.of({ count })
-              : P.whitespace
-                  .then(P.regex(/in pale/i).result('pale' as const))
-                  .map((disposition) => ({ count, disposition })),
-            P.whitespace.then(tinctureParserFromName),
-            P.whitespace
-              .then(P.regex(/armed and langued/i))
-              .then(P.whitespace)
-              .then(tinctureParserFromName)
-              .fallback(null)
-          ).map(([attitude, head, tail, countAndDisposition, tincture, armedAndLangued]) => {
-            return {
-              name: 'lion',
-              attitude,
-              tincture,
-              armedAndLangued: armedAndLangued || gules,
-              tail,
-              head,
-              countAndDisposition,
-            };
-          });
-
-          return lionParser;
+          return lionParser(count);
         } else if (charge === 'eagle') {
-          const eagleNameParser = count === 1 ? P.regex(/eagle/i) : P.regex(/eagles/i);
-          const eagleAttitudeParser: P.Parser<EagleAttitude> = buildAltParser(eagleAttitudes, identity);
-
-          return P.seq(
-            eagleNameParser.skip(P.whitespace).then(eagleAttitudeParser),
-            P.whitespace.then(tinctureParserFromName),
-            P.whitespace
-              .then(P.regex(/beaked and armed/i))
-              .then(P.whitespace)
-              .then(tinctureParserFromName)
-              .fallback(null)
-          ).map(
-            ([attitude, tincture, beakedAndArmed]): Eagle => {
-              return {
-                name: 'eagle',
-                attitude,
-                tincture,
-                beakedAndArmed: beakedAndArmed || tincture,
-              };
-            }
-          );
+          return eagleParser(count);
         } else {
           return cannotHappen(charge);
         }
