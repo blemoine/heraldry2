933cb5d5b09d6e92646672e88d38902bb5d9a8a1
Benoit Lemoine <blemoine@mnubo.com>
2019-07-26 21:04:53 -0400

    refactor(chevron): use path builder

--- src/app/from-blason/coats-of-arms-parts/fields/BendyDisplay.tsx
+++ src/app/from-blason/coats-of-arms-parts/fields/BendyDisplay.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Dimension } from '../../../model/dimension';
+import { SvgPathBuilder } from '../../../svg-path-builder/svg-path-builder';
+import { PathFromBuilder } from '../../../common/PathFromBuilder';
 
 type Props = { fill: [string, string]; dimension: Dimension };
 export const BendyDisplay: React.FunctionComponent<Props> = (props) => {
@@ -9,41 +11,47 @@ export const BendyDisplay: React.FunctionComponent<Props> = (props) => {
 
   const firstPoint = 1 / 4;
   const secondPoint = 1 / 2;
+
+  const path1 = SvgPathBuilder.start([0, 0])
+    .goTo([width * firstPoint, 0])
+    .goTo([width, height * (1 - firstPoint)])
+    .goTo([width, height])
+    .close();
+
+  const path2 = SvgPathBuilder.start([width * firstPoint, 0])
+    .goTo([width * secondPoint, 0])
+    .goTo([width, height * (1 - secondPoint)])
+    .goTo([width, height * (1 - firstPoint)])
+    .close();
+
+  const path3 = SvgPathBuilder.start([width * secondPoint, 0])
+    .goTo([width, 0])
+    .goTo([width, height * (1 - secondPoint)])
+    .close();
+
+  const path4 = SvgPathBuilder.start([0, 0])
+    .goTo([0, height * firstPoint])
+    .goTo([width * (1 - firstPoint), height])
+    .goTo([width, height])
+    .close();
+  const path5 = SvgPathBuilder.start([0, height * firstPoint])
+    .goTo([0, height * secondPoint])
+    .goTo([width * firstPoint, height])
+    .goTo([width * (1 - firstPoint), height])
+    .close();
+  const path6 = SvgPathBuilder.start([0, height * secondPoint])
+    .goTo([0, height])
+    .goTo([width * (1 - secondPoint), height])
+    .close();
+
   return (
     <g>
-      <path
-        d={`M 0 0 H ${width * firstPoint} L ${width} ${height * (1 - firstPoint)} V ${height} Z`}
-        fill={fill[0]}
-        stroke="#333"
-      />
-      <path
-        d={`M ${width * firstPoint} 0 H ${width * secondPoint} L ${width} ${height * (1 - secondPoint)} V ${height *
-          (1 - firstPoint)} Z`}
-        fill={fill[1]}
-        stroke="#333"
-      />
-      <path
-        d={`M ${width * secondPoint} 0 H ${width} V ${height * (1 - secondPoint)} Z`}
-        fill={fill[0]}
-        stroke="#333"
-      />
-
-      <path
-        d={`M 0 0 V ${height * firstPoint} L ${width * (1 - firstPoint)} ${height} H ${width} Z`}
-        fill={fill[1]}
-        stroke="#333"
-      />
-      <path
-        d={`M 0 ${height * firstPoint} V ${height * secondPoint} L ${width * firstPoint} ${height} H ${width *
-          (1 - firstPoint)} Z`}
-        fill={fill[0]}
-        stroke="#333"
-      />
-      <path
-        d={`M 0 ${height * secondPoint} V ${height} H ${width * (1 - secondPoint)} Z`}
-        fill={fill[1]}
-        stroke="#333"
-      />
+      <PathFromBuilder pathBuilder={path1} fill={fill[0]} stroke="#333" />
+      <PathFromBuilder pathBuilder={path2} fill={fill[1]} stroke="#333" />
+      <PathFromBuilder pathBuilder={path3} fill={fill[0]} stroke="#333" />
+      <PathFromBuilder pathBuilder={path4} fill={fill[1]} stroke="#333" />
+      <PathFromBuilder pathBuilder={path5} fill={fill[0]} stroke="#333" />
+      <PathFromBuilder pathBuilder={path6} fill={fill[1]} stroke="#333" />
     </g>
   );
 };
--- src/app/from-blason/coats-of-arms-parts/fields/ChevronDisplay.tsx
+++ src/app/from-blason/coats-of-arms-parts/fields/ChevronDisplay.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import { Dimension } from '../../../model/dimension';
+import { SvgPathBuilder } from '../../../svg-path-builder/svg-path-builder';
+import { PathFromBuilder } from '../../../common/PathFromBuilder';
 
 type Props = { fill: [string, string]; dimension: Dimension };
 export const ChevronDisplay: React.FunctionComponent<Props> = ({ dimension: { width, height }, fill }) => {
+  const pathBuilder = SvgPathBuilder.start([-width / 3, height])
+    .goTo([width / 2, height / 3])
+    .goTo([(width * 4) / 3, height])
+    .close();
+
   return (
     <g>
       <rect x={0} y={0} height={height} width={width} fill={fill[0]} stroke="#333" />
-      <path
-        d={`M ${-width / 3} ${height} L ${width / 2} ${height / 3} L ${(width * 4) / 3} ${height}  Z`}
-        fill={fill[1]}
-        stroke="#333"
-      />
+      <PathFromBuilder pathBuilder={pathBuilder} fill={fill[1]} stroke="#333" />
     </g>
   );
 };
