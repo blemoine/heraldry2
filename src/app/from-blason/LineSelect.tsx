bc98e41709e758b7aa21deba29995d5d13debbf8
Benoit Lemoine <blemoine@mnubo.com>
2019-07-28 11:18:36 -0400

    feat(ordinary): add line selector

---
+++ src/app/from-blason/LineSelect.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Line, lines } from '../model/line';
+import { SelectScalar } from '../common/SelectScalar';
+
+type Props = { line: Line; lineChange: (line: Line) => void };
+export const LineSelect = (props: Props) => {
+  return <SelectScalar options={lines} value={props.line} valueChange={props.lineChange} />;
+};
--- src/app/from-blason/ordinaries/StandardOrdinaryForm.tsx
+++ src/app/from-blason/ordinaries/StandardOrdinaryForm.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import { Ordinary, Pale } from '../../model/ordinary';
+import { Bordure, Chief, Ordinary, Pale } from '../../model/ordinary';
 import { TinctureSelect } from '../TinctureSelect';
 import { Tincture } from '../../model/tincture';
+import { LineSelect } from '../LineSelect';
+import { Line } from '../../model/line';
 
 type SupportedOrdinary = Exclude<Ordinary, Pale>;
 type Props = { ordinary: SupportedOrdinary; ordinaryChange: (ordinary: SupportedOrdinary) => void };
 export const StandardOrdinaryForm = ({ ordinary, ordinaryChange }: Props) => {
-  function ordinaryTinctureChange(ordinaryTincture: Tincture) {
-    ordinaryChange({
-      ...ordinary,
-      tincture: ordinaryTincture,
-    });
+  function ordinaryTinctureChange(tincture: Tincture) {
+    ordinaryChange({ ...ordinary, tincture });
+  }
+
+  function lineChange(ordinary: Chief | Bordure, line: Line) {
+    ordinaryChange({ ...ordinary, line });
   }
 
   return (
@@ -19,6 +22,8 @@ export const StandardOrdinaryForm = ({ ordinary, ordinaryChange }: Props) => {
         <label>Select the tincture of the ordinary</label>
         <TinctureSelect tincture={ordinary.tincture} tinctureChange={ordinaryTinctureChange} />
       </div>
+
+      {'line' in ordinary && <LineSelect line={ordinary.line} lineChange={(line) => lineChange(ordinary, line)} />}
     </>
   );
 };
