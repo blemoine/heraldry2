36f314a426bccfc27e24b0f7404cc167ba84e43b
Benoit Lemoine <blemoine@mnubo.com>
2019-08-02 19:07:39 -0400

    refactor: simplify blason model

--- src/app/model/blason.ts
+++ src/app/model/blason.ts
@@ -2,7 +2,7 @@ import { Ordinary } from './ordinary';
 import { Charge } from './charge';
 import { Field } from './field';
 
-export type Blason = { field: Field } & {
+export type Blason = {
   field: Field;
   ordinary?: Ordinary;
   charge?: Charge;
