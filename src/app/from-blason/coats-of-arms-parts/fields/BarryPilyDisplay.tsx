b13ad0bd50b78e9175e8287af2f21d319850b7ad
Benoit Lemoine <blemoine@mnubo.com>
2019-10-05 08:31:03 -0400

    fix(barryPoliy): each side should touch the opposite

--- src/app/from-blason/coats-of-arms-parts/fields/BarryPilyDisplay.tsx
+++ src/app/from-blason/coats-of-arms-parts/fields/BarryPilyDisplay.tsx
@@ -6,13 +6,12 @@ import { range } from '../../../../utils/range';
 
 type Props = { fill: [string, string]; dimension: Dimension };
 export const BarryPilyDisplay: React.FunctionComponent<Props> = ({ dimension: { width, height }, fill }) => {
-  const scaledWidth = width * 0.8;
   const numberOfPale = 4;
 
   const pathBuilder = range(0, numberOfPale)
     .reduce((pathBuilder, i) => {
       return pathBuilder
-        .goTo([scaledWidth, ((2 * i + 1) * height) / (2 * numberOfPale)])
+        .goTo([width, ((2 * i + 1) * height) / (2 * numberOfPale)])
         .goTo([0, ((2 * i + 2) * height) / (2 * numberOfPale)]);
     }, SvgPathBuilder.start([0, 0]))
     .close();
