d6a5724582402a67d6c81fca00c6e4965c773a6d
Benoit Lemoine <blemoine@mnubo.com>
2019-10-31 19:11:58 -0400

    fix: display the ordinary name

--- src/app/from-blason/form/OrdinaryNameSelect.tsx
+++ src/app/from-blason/form/OrdinaryNameSelect.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { isSubOrdinary, ordinaries, Ordinary } from '../../model/ordinary';
 import { SelectScalar } from '../../common/SelectScalar';
+import { stringifyOrdinaryName } from '../../model/stringify/stringify.helper';
 
 type Props = { ordinary: Ordinary['name'] | null; ordinaryChange: (t: Ordinary['name'] | null) => void };
 type OrdinaryNameOrNone = Ordinary['name'] | 'None';
 
+const stringFormatter = (o: Ordinary['name'] | 'None'): string => (o === 'None' ? o : stringifyOrdinaryName(o));
 const trueOrdinary = ordinaries.filter((o) => !isSubOrdinary(o));
 const subOrdinaries = ordinaries.filter(isSubOrdinary);
 const options: { [group: string]: Array<OrdinaryNameOrNone> } = {
@@ -19,5 +21,12 @@ export const OrdinaryNameSelect = ({ ordinary, ordinaryChange }: Props) => {
 
   const searchedOrdinary = ordinary || 'None';
 
-  return <SelectScalar options={options} value={searchedOrdinary} valueChange={ordinaryTypeChange} />;
+  return (
+    <SelectScalar
+      options={options}
+      value={searchedOrdinary}
+      valueChange={ordinaryTypeChange}
+      formatValue={stringFormatter}
+    />
+  );
 };
