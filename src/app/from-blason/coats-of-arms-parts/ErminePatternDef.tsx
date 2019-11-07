b0b35ca28913de4afe893c3053a70159e3f4c22b
Benoit Lemoine <blemoine@mnubo.com>
2019-11-07 18:08:26 -0500

    chore(ermine): simplify the pattern

--- src/app/from-blason/coats-of-arms-parts/ErminePatternDef.tsx
+++ src/app/from-blason/coats-of-arms-parts/ErminePatternDef.tsx
@@ -9,7 +9,6 @@ export const ErminePatternDef = ({ ermine, patternId, dimension: { width }, tinc
   const fieldColor = tinctureConfiguration[ermine.field.name];
   const patternUnitWidth = 70;
   const patternUnitHeight = 125;
-  const patternHeightMargin = 5;
   const patternWidth = width / 5.5;
   const patternHeight = (patternWidth * patternUnitHeight) / patternUnitWidth;
   const id = ermine.name + '-' + ermine.field.name + '-' + ermine.spot.name;
@@ -27,19 +26,13 @@ export const ErminePatternDef = ({ ermine, patternId, dimension: { width }, tinc
         viewBox={`0 0 ${patternUnitWidth} ${patternUnitHeight}`}
       >
         <rect width={patternUnitWidth} height={patternUnitHeight} fill={fieldColor} />
-        <use
-          href={'#' + id}
-          x="0"
-          y="0"
-          width={patternUnitWidth / 2}
-          height={(patternUnitHeight - patternHeightMargin) / 2}
-        />
+        <use href={'#' + id} x="0" y="0" width={patternUnitWidth / 2} height={patternUnitHeight / 2} />
         <use
           href={'#' + id}
           x={patternUnitWidth / 2}
-          y={(patternUnitHeight + patternHeightMargin) / 2}
+          y={patternUnitHeight / 2}
           width={patternUnitWidth / 2}
-          height={(patternUnitHeight - patternHeightMargin) / 2}
+          height={patternUnitHeight / 2}
         />
       </pattern>
     </>
