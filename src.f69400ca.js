77804750dbfd97c8a22b80050f98b8384512549f
Benoit Lemoine <blemoine@mnubo.com>
2019-11-17 14:08:14 -0500

    Updates

--- src.f69400ca.js
+++ src.f69400ca.js
@@ -91972,7 +91972,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50732" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51087" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
