1217aab3e2c914574ce7381d1e25c0a8c0831ddd
Antoine LUONG <antoine.luong@c-s.fr>
2020-04-14 12:00:39 +0200

    Allow imports relative to the root dir

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css|less)$': 'identity-obj-proxy',
+    '~(.*)': '<rootDir>/src/$1',
   },
   modulePathIgnorePatterns: ['node_modules'],
   setupFiles: ['./src/utils/tests/jest-polyfill.js'],
